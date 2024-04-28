bd01a791d13ebe5ed4f71403ee651acd7cf3449c
AsserElshafey <elshafeyasser@yahoo.com>
2024-04-29 01:12:39 +0300

    HOT FIX

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -44,9 +44,10 @@ export const PATCH = async (request, { params }) => {
 export const DELETE = async (request, { params }) => {
   try {
     await connectToDB();
+    console.log(params.id);
 
     // Find the prompt by ID and remove it
-    await Prompt.findByIdAndRemove(params.id);
+    await Prompt.findByIdAndDelete(params.id);
 
     return new Response("Prompt deleted successfully", { status: 200 });
   } catch (error) {
