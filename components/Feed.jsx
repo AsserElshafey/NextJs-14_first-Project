ea9c572b9ded7b0a2d1ad0c9225648d9caf85644
AsserElshafey <elshafeyasser@yahoo.com>
2024-04-29 18:59:19 +0300

    HOT FIX

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -28,9 +28,9 @@ const Feed = () => {
 
   const fetchPosts = async () => {
     const response = await fetch("/api/prompt", {
-      headers: {
-        'Cache-Control': 'no-store'
-      }
+
+      cache: 'no-store'
+
     });
     const data = await response.json();
 
